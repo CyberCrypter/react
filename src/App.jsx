71178a9ca2e23eed8add1fa9cb68b30824b0296f
Jiten <jitendas9574@gmail.com>
2025-11-04 20:47:31 +0530

    Form handling using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,22 @@
-import React, { useState } from 'react'
-import Card from './components/Card'
-import Navbar from './components/Navbar'
+import React, { useRef } from 'react'
 
 const App = () => {
 
-const data = [
-  {image: "https://plus.unsplash.com/premium_photo-1673292293042-cafd9c8a3ab3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=1000", name: "Believer", artist: "cherry", added: false},
-  {image: "https://images.unsplash.com/photo-1518495973542-4542c06a5843?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687", name: "chaiya", artist: "shah rukh", added: false},
-  {image: "https://plus.unsplash.com/premium_photo-1675827055694-010aef2cf08f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=712", name: "sukoon", artist: "sulai saleman", added: false},
-  {image: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=974", name: "attention", artist: "jiten", added: true}
-];
+  const name = useRef(null);
+  const age = useRef(null);
 
-const [songData, setSongData] = useState(data);
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    console.log(name.current.value, age.current.value);
+    
 
-const handleClick = (index) => {
-  setSongData((prev)=>{
-    return prev.map((item, itemindex)=>{
-      if(itemindex === index) return {...item, added: !item.added};
-      return item;
-    })
-  })
-
-}
-
+  }
   return (
-    <>
-      <div className='w-full h-screen bg-zinc-300'>
-          <Navbar data={songData}/>
-          <div className='px-20 flex gap-10 mt-10 flex-wrap'>
-            {songData.map((obj,index)=> (
-              <Card data={obj} handleClick={handleClick} index={index} key={index}/>
-              ))}
-          </div>
-      </div>
-    </>
+    <form action="" onSubmit={handleSubmit}>
+      <input ref={name} placeholder='name' type="text" />
+      <input ref={age} placeholder='age' type="text" />
+      <input type="submit" />
+    </form>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
 
-const Card = ({data, handleClick, index}) => {
-    const {image, artist, added, name} = data;
+const Card = () => {
   return (
-    <div className='w-60 bg-zinc-100 p-4 rounded-md flex gap-4 pb-8 relative mt-10'>
-        <div className='w-20 h-20 bg-orange-600 rounded-md overflow-hidden'>
-            <img className='w-full h-full object-cover' src={image} alt="" />
-        </div>
-        <div className=''>
-            <h3 className='text-xl leading-none font-semibold'>{name}</h3>
-            <h6 className='text-sm'>{artist}</h6>
-        </div>
-        <button onClick={()=>handleClick(index)} className={`px-4 py-3 whitespace-nowrap bottom-0 left-1/2 -translate-x-[50%] translate-y-[50%] ${added === false ? "bg-orange-600" : "bg-teal-700"} absolute text-white text-xs rounded-full cursor-pointer`}>{added === false ? "Add to Favourites" : "Added"}</button>
-    </div>
+    <div>Card</div>
   )
 }
 
