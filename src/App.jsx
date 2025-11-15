46dc72e000830143ab33d52906a9a0940a3b05be
Jiten <jitendas9574@gmail.com>
2025-11-15 17:17:28 +0530

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
-import { Link, Route, Routes} from "react-router-dom";
-import Form from './components/Form';
-import Card from './components/Card';
-import Cards from './components/Cards';
-const App = () => {
-  return (
-    <>
-    <nav>
-      <Link to="/form">Form</Link>
-      <Link to="/card">Card</Link>
-      <Link to="/cards">go to Cards</Link>
-    </nav>
-      <Routes>
-          <Route path="/form" element={<Form />} />
-          <Route path="/card" element={<Card />} />
-          <Route path="/cards" element={<Cards />} />
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement, incrementByAmount } from './store/reducer/counterSlice'
 
-      </Routes>
-    </>
+
+function App() {
+  const {value} = useSelector((state)=> state.counter)
+  const dispatch = useDispatch();
+  
+  return (
+    <div>
+      <h1 className='text-center p-4 font-bold text-4xl'>Counter: {value}</h1>
+
+      <div className='text-center pt-12 flex-col'>
+        <button onClick={()=> dispatch(increment())} className='text-2xl border-b-amber-950 border-3 bg-amber-200 mr-2 cursor-pointer p-2 rounded-md font-medium'>increment</button>
+        <button onClick={()=> dispatch(decrement())} className='text-2xl border-b-amber-950 border-3 bg-amber-200 mr-2 cursor-pointer p-2 rounded-md font-medium'>decrement</button>
+        <button onClick={()=> dispatch(incrementByAmount(5))} className='text-2xl border-b-amber-950 border-3 bg-amber-200 p-2 cursor-pointer rounded-md font-medium'>increment by 5</button>
+      </div>
+    </div>
   )
 }
 
